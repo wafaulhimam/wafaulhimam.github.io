4b817c89c0929d4227f19cb47d3971a88f36efec
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-12-08 18:06:58 +0700

    edit main js

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends Component {
     return (
         <BrowserRouter>
         <div>
-          <h1>Simple SPA</h1>
+          <h1>Wafa Home</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
