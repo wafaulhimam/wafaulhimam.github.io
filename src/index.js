a514021e677494fae269e51e4cb0e52f4063357b
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-12-08 19:57:03 +0700

    reset react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./Main";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
- 
-ReactDOM.render(
-  <Main/>, 
-  document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
