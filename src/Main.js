bb8ea5bad56d60f3393b0948e2f40673323a2d9d
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-12-08 16:41:40 +0700

    insert react spa

---
+++ src/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    BrowserRouter
+} from 'react-router-do m';
+import Home from './Home';
+import Stuff from './Stuff';
+import Contact from './Contact';
+ 
+class Main extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+             
+          </div>
+        </div>
+        </BrowserRouter>
+    );
+  }
+}
+ 
+export default Main;
